4c59535abcdc27cf5d1e6df97a10405da1711ebb
IndiraJayanti <165116625+IndiraJayanti@users.noreply.github.com>
2024-11-05 09:41:12 +0800

    add script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('greetButton').addEventListener('click', function() {
+    const greetingMessage = document.getElementById('greetingMessage');
+    greetingMessage.textContent = 'Halo! Terima kasih telah mengunjungi halaman ini.';
+});
